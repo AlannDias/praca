856780ec848d2b2420a8914ee13d5a337b139b54
Alann de Oliveira DIas <alanndias13@gmail.com>
2015-11-09 00:58:18 -0300

    projeto praca

---
+++ pagina2.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<head>
+	
+	
+	<link rel="stylesheet" href="images/style.css" type="text/css" />
+
+	<html xmlns="http://www.w3.org/1999/xhtml" lang="pt-br" xml:lang="pt-br">
+	<?xml version="1.0" encoding="utf-8" ?>
+	<title>Acesso WIFI</title>
+    <style type="text/css">
+<!--
+.style1 {color: #000000}
+.style2 {color: #FF0000}
+.style3 {color: #0000FF}
+-->
+    </style>
+</head>
+<body>
+	<div id="page" align="center">
+	  <div id="header">
+			<div id="companyname" align="left"></div>
+			<div align="right" class="links_menu" id="menu"><a href="http://www.fiepb.com.br">Informatica / Federação das Industrias do Estado da Paraiba.</a> </div>
+	  
+		
+		
+
+				<div id="leftpanel">
+				  
+					<br />
+
+
+					
+						
+							<div class="table_text">
+							
+								
+						
+							<img src="images/professorprego.png" id="moldurafoto" width="320" height="260"  alt="aqui é que vai o texto que vai aparecer quando vc passar o mouse por cima!" /> 
+							<div id= "imagemtexto">
+							<img src="images/textoprofessor3.jpg" id="fototexto" width="430" height="220" </div>
+							</div>
+						
+						
+
+						
+						
+					    
+				</div>
+					<div id="contenttext">
+						<div align="left">
+						 
+                              
+						  	  
+					    </div>
+					    
+						<div id="fonte"><h6>Fonte: Relembrando José Lopes de Andrade, CG-Retalhos Históricos, Acesso em: 23/07</h6></div>
+				    </div>
+
+			    <br />
+			    <br />
+		       <div class="footer">
+				<br />
+				<a href="#">Praça Professor Lopes de Andrade</a> </a></a></div>
+	  </div>
+	</div>
+</body>
+</html>
